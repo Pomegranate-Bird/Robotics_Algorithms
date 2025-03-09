1dd9c93ecfe2334e70c44875bcc4bb6526e1c6a7
Marvin Cruz Gomez <cruzmarvin447@gmail.com>
2025-03-09 15:35:35 -0700

    day 1 progress on POGM for project

--- Algorithms/probabilistic_ogm.py
+++ Algorithms/probabilistic_ogm.py
@@ -1,16 +1,31 @@
 import numpy as np
-from numpy.random import default_rng
+import random
 
-GRID_Height = 100
-GRID_WIDTH  = 100
+GRID_Height = 10
+GRID_WIDTH  = 10
 INIT_UNCERTAINTY = 0.5 
+OCCUPIED = 1 
+UNOCCUPIED = 0
 
 #Intializing an Occupancy Grid map 
 grid = np.zeros((GRID_Height,GRID_WIDTH)) 
 
 
-#Assigning some probabilities to the Occuopancy Grid Map 
+#Assigning all cells an intial uncertainty to the Occuopancy Grid Map 
+grid[:, :] = INIT_UNCERTAINTY
 
-grid[:, :] = 0.5
+#Creating a wall within the map(occupied cells)
+wall = grid[3:, 3] = OCCUPIED
+staticmethod = wall # Static object within the envioremnt 
+
+# Scattering random objects throughout the map
+deafult_rng = np.random.default_rng() 
+random_probabilities = deafult_rng.random()
+
+#Appending some of the random values to a section of the array 
+
+objects = grid[:, :] = random_probabilities
+
+staticmethod = objects # Making the objects static for now
 
 print(grid)
