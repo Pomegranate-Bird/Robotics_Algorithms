4086a863ba38180d143255a1865d2ca4575cd77b
Marvin Cruz Gomez <cruzmarvin447@gmail.com>
2025-03-08 21:58:17 -0800

    Just creating a grid for visualizing OGM

---
+++ ogm_grid_visualization.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Creating a 4x4 grid, filled with 0.5 grey 
+grid = np.full((4,4), 1)
+
+# Display the grid
+# cmap, sets the color map 
+# vmin and max define the value range for the color scale, 0 is black and 1 is white
+
+plt.imshow(grid, cmap = 'gray', vmin = 0, vmax=1)
+
+# Add grid lines for better visibility 
+plt.grid(visible = True, color = 'black', linewidth = 2)
+
+# Set tick positions to align with cell borders
+# np.arange(-0.5, 4, 1) sets tick marks starting at -0.5, ending at 3.5, with a step of 1
+# Empty lists [] remove the numeric labels for cleaner visualization
+
+plt.xticks(np.arange(-0.5,4,1), [])
+plt.yticks(np.arange(-0.5,4,1), [])
+
+plt.show()
